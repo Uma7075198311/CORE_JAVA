dba7e00caacd16e651702222c2e98969a7a47ebb
Kumam <kumamaheswari243@gmail.com>
2026-01-25 18:39:14 +0530

    Variables task

---
+++ src/variables/Task20.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Task20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  String name = "Kalal Swathika";
+	        int age = 22;
+	        String course = "Computer Science";
+	        String college = "ABC Engineering College";
+	        String email = "swathika@gmail.com";
+	        String bio = "Name: " + name +
+	                     "\nAge: " + age +
+	                     "\nCourse: " + course +
+	                     "\nCollege: " + college +
+	                     "\nEmail: " + email;
+	        System.out.println(bio);
+	}
+
+}
